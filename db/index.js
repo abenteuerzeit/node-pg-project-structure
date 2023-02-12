2cbcd31900c0a446517d8cbc4c25b96d26d836a1
abenteuerzeit <amroz.muzyka@gmail.com>
2023-02-12 10:59:31 +0100

    async diagnostic info

--- db/index.js
+++ db/index.js
@@ -7,44 +7,41 @@ const { Pool } = require('pg');
 const pool = new Pool();
 
 module.exports = {
-    async query: (text, params, callback) => {
+    async query (text, params) {
         // logging for every executed query
         // query parameters are not logged to protect against exposing sensitive information such as encrypted passwords
         const startTime = Date.now();
-        return pool.query(text, params, (err, res) => {
-            const duration = Date.now() - startTime;
-            console.log('executed query', { text, duration, rows: res.rowCount });
-            callback(err, res);
-        });
+        const res = await pool.query(text, params);
+        const duration = Date.now() - startTime;
+        console.log('executed query', { text, duration, rows: res.rowCount });
+        return res;
     },
     getClient: (callback) => {
-        pool.connect((err, client, done) => {
-            const query = client.query;
+        const client = await pool.connect();
+        const query = client.query;
+        const release = client.release;
+        
+        // set a timeout of 5 seconds, after which we will log this client's last query
+        const timeout = setTimeout(() => {
+            console.error('A client has been checked out for more than 5 seconds!');
+            console.error(`The last executed query on this client was: ${client.lastQuery}`);
+        }, 5000);
 
-            // monkey patch the query method to keep track of the last query executed
-            client.query = (...args) => {
-                client.lastQuery = args;
-                return query.apply(client, args);
-            };
+        // monkey patch the query method to keep track of the last query executed
+        client.query = (...args) => {
+            client.lastQuery = args;
+            return query.apply(client, args);
+        };
 
-            // set a timeout of 5 seconds, after which we will log this client's last query
-            const timeout = setTimeout(() => {
-                console.error('A client has been checked out for more than 5 seconds!');
-                console.error(`The last executed query on this client was: ${client.lastQuery}`);
-            }, 5000);
+        client.release = () => {
+            // clear the timeout
+            clearTimeout(timeout);
+            // set the methods back to their old un-monkey-patched version
+            client.query = query;
+            client.release = release;
+            return release.apply(client);
+        }
 
-            const release = (err) => {
-                // call the actual 'done' method, returning this client to the pool
-                done(err);
-
-                // clear our timeout
-                clearTimeout(timeout);
-
-                // set the query method back to its old un-monkey-patched version
-                client.query = query;
-            };
-
-            callback(err, client, release)
-        });
+        return client;
     },
 };
